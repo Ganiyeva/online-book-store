2182b4ea4e247ba08b33f42495a9d756ba8583e6
rabonas <86463452+rabonas@users.noreply.github.com>
2021-11-08 11:31:30 +0500

    added sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import './Sidebar.css';
+import React from 'react';
 
 const Sidebar = () => {
     return (
-        <div>
-            
-        </div>
+        <aside className="sidebar">
+            <div className="cart">
+                <i className="icon-shopping_basket"></i>
+                <span className="shopping-count">0</span>
+            </div>
+            <div className="search bottom-icon">
+                <i className="icon-search"></i>
+            </div>
+            <div className="contact bottom-icon">
+                <i className="icon-person"></i>
+            </div>
+            <div className="customization bottom-icon">
+                <i className="icon-cog"></i>
+            </div>
+        </aside>
     )
 }
 
