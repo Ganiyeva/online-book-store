3c90d2b943703a2c687212ae3f6c8bb9d652ba6c
BintuNajmiddin <86461130+BintuNajmiddin@users.noreply.github.com>
2021-11-08 10:51:46 +0500

    wrote nav in header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App"> </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/readbook">
+            <ReadBook />
+          </Route>
+          <Route path="/filter">
+            <Filter />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
