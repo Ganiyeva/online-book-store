108d8352509d1d5d759214e13bbb42e6f07a4467
Ganiyeva <86459763+Ganiyeva@users.noreply.github.com>
2021-11-08 12:02:29 +0500

    ddd

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,12 +1,18 @@
- import React from 'react'
+import SwiperCore, {Autoplay} from 'swiper';
+import {Swiper, SwiperSlide} from "swiper/react";
  
  const Slider = () => {
-     return (
-         <div>
-             
-         </div>
-     )
- }
+  SwiperCore.use([Autoplay]);
+
+    return (
+      <div>
+        <Swiper modules={[Autoplay]} grabCursor={true} spaceBetween={0} slidesPerView={1} loop autoplay={{delay: 5000, disableOnInteraction: false}}>
+          <SwiperSlide>
+            <img src="img/slide1.jpg" alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    );
+ };
  
- export default Slider
- 
+ export default Slider;
