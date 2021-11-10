0ffbb399901bd0f658a77f4eeca645e07e2a0dc4
BintuNajmiddin <86461130+BintuNajmiddin@users.noreply.github.com>
2021-11-10 09:48:16 +0500

    added icon for footer

--- src/components/HEADER/Header.js
+++ src/components/HEADER/Header.js
@@ -4,14 +4,7 @@ import {Link}  from 'react-router-dom';
 const Header = () => {
     return(
         <div>
-            <img src="img/book-logo.png" alt="book-logo"/>
-            <div className="container">
-                <nav>
-                    <Link to="/src/pages/Home.js">Home</Link>
-                    <Link to="/src/pages/Filter.js">Filter</Link>
-                    <Link to="/src/pages/ReadBook.js">ReadBook</Link>
-                </nav>
-            </div>
+            
         </div>
     );
 };
