17c790f41454fcdd5861d1cdee630466f795fa2c
Ganiyeva <86459763+Ganiyeva@users.noreply.github.com>
2021-11-11 13:52:07 +0500

    api key

---
+++ src/global.js
@@ -0,0 +1,4 @@
+export const GOOGLE_API_KEY = 'AIzaSyBEHIje4bmaXSixVqZ9HXZ0bqDBnOUmnxc';
+export const GOOGLE_API = `https://www.googleapis.com/books/v1/volumes?q=flower&key=${GOOGLE_API_KEY}`;
+export const NYTIMES_API_KEY = 'caf84bdb-c39b-45fe-b2a9-12ed9a889b2d';
+export const NYTIMES_API = `https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=${NYTIMES_API_KEY}`;
