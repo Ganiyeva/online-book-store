cc29d2f5be2715a683a92cb0fde28cdf880f17c4
Ganiyeva <86459763+Ganiyeva@users.noreply.github.com>
2021-11-11 20:25:12 +0500

    remove bookCard and bookList

--- src/components/Books/Booklist.js
+++ src/components/Books/Booklist.js
@@ -1,9 +1,31 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, {Navigation} from 'swiper';
+import { useState, useEffect } from "react";
+import { NYTIMES_API } from "../../global";
 import BookCard from "./Bookcard";
+import "./booklist.css";
 
 const BookList = () => {
+
+  SwiperCore.use([Navigation]);
+  const [bookList, setBookList] = useState([]);
+
+  useEffect(() => {
+    fetch(NYTIMES_API).then(res => res.json()).then(data => {
+      setBookList(data.results);
+      console.log(data.results);
+    })
+  }, [])
+
   return (
-    <div className="container">
-      <BookCard />
+    <div className="book-content">
+      <div className="container">
+        <div className="slider-content">
+          <Swiper modules={Navigation} navigation={true} pagination={{ clickable: true }} grabCursor={true} spaceBetween={0} slidesPerView={8}>
+              {bookList.map(el => (<SwiperSlide key={el.id} > <BookCard bookObj={el} /> </SwiperSlide>))}
+          </Swiper>
+        </div>
+      </div>
     </div>
   );
 };
