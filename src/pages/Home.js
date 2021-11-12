559da4959cbab220e06d7ca17a6eb686829bd9f8
BintuNajmiddin <86461130+BintuNajmiddin@users.noreply.github.com>
2021-11-12 12:34:15 +0500

    merged

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,13 @@
 import Advertising from "../components/Advertising/Advertising";
-// import BookList from "../components/Books/Booklist";
+import BookList from "../components/Books/Booklist";
 
 const Home = () => {
   return (
     <div>
       <Advertising />
-<<<<<<< HEAD
-      {/* <BookList /> */}
-=======
       <BookList type='love' title='Love' />
       <BookList type='feminism' title='Feminism' />
       <BookList type='inspirational' title='Inspirational' />
->>>>>>> ea59c547b3b8252a4aea0815c638b788500c9cd7
     </div>
   );
 };
