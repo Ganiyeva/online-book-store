ba2275d7ed29e25f7e99abd1139531343233a62d
Ganiyeva <86459763+Ganiyeva@users.noreply.github.com>
2021-11-12 12:21:46 +0500

    remove API

--- src/components/Books/Bookcard.js
+++ src/components/Books/Bookcard.js
@@ -1,32 +1,16 @@
 import { Rate } from 'antd';
 import "./bookcard.css";
-import { useEffect, useState } from 'react';
-import {GOOGLE_API_KEY} from "../../global";
-
-const GOOGLE_BOOK = `https://www.googleapis.com/books/v1/volumes?q=isbn:0399178570&key=${GOOGLE_API_KEY}`;
 
 const BookCard = ({bookObj}) => {
-
-  const [image, setImage] = useState('');
-
-  useEffect(() => {
-    if(bookObj.isbns && bookObj.isbns.length > 0) {
-      const isbnCode = bookObj.isbns[0].isbn10;
-      console.log(isbnCode)
-      fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbnCode}&key=${GOOGLE_API_KEY}`).then(res => res.json()).then(data => {
-        if(data.items) setImage(data.items[0].volumeInfo.imageLinks?.thumbnail);
-      });
-    }
-  }, []);
 
   return (
     <div className="card">
       <div>
-        {bookObj.isbns !== [] ? <img className="image" src={image} alt={bookObj.title} /> : <div></div> }
+        {bookObj.imageLinks ? <img className="image" src={bookObj.imageLinks.smallThumbnail} alt={bookObj.title} /> : <div></div>}
       </div>
       <Rate />
       <h4 className="name"> {bookObj.title} </h4>
-      <h5 className="author"> {bookObj.author} </h5>
+      <h5 className="author"> {bookObj.authors} </h5>
     </div>
   )
 }
